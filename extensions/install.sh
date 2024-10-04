5d3900eda0e6c54f2b311a13e1667d365d8e17c0
Trevor Terris <tkterris@gmail.com>
2024-10-04 15:28:37 -0400

    Fix install.sh

--- extensions/install.sh
+++ extensions/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# Runs during image build, used to e.g. copy custom module libraries
 # See https://docs.redhat.com/en/documentation/red_hat_jboss_enterprise_application_platform/8.0/html-single/using_jboss_eap_on_openshift_container_platform/index#using_install_sh_to_execute_custom_scripts
+set -x
 echo "Running $PWD/install.sh"
+injected_dir=$1
+# copy any needed files into the target build.
+cp -rf ${injected_dir} $JBOSS_HOME/extensions
