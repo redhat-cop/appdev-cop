56db217f5b7ab6cbeb451c5aba2a214b0f1e3337
Trevor Terris <tkterris@gmail.com>
2025-08-20 13:41:51 -0400

    Bump version to ISPN 15 / RHDG 8.5

--- rhdg-ocp/server/src/main/java/com/redhat/rhdg/demo/server/loader/CustomStoreConfiguration.java
+++ rhdg-ocp/server/src/main/java/com/redhat/rhdg/demo/server/loader/CustomStoreConfiguration.java
@@ -5,17 +5,18 @@ import org.infinispan.commons.configuration.ConfigurationFor;
 import org.infinispan.commons.configuration.attributes.AttributeSet;
 import org.infinispan.configuration.cache.AbstractStoreConfiguration;
 import org.infinispan.configuration.cache.AsyncStoreConfiguration;
+import org.infinispan.configuration.parsing.Element;
 
 @BuiltBy(CustomStoreConfigurationBuilder.class)
 @ConfigurationFor(CustomStore.class)
-public class CustomStoreConfiguration extends AbstractStoreConfiguration {
+public class CustomStoreConfiguration extends AbstractStoreConfiguration<CustomStoreConfiguration> {
 
 	public static AttributeSet attributeDefinitionSet() {
 		return new AttributeSet(CustomStoreConfiguration.class, AbstractStoreConfiguration.attributeDefinitionSet());
 	}
 
 	public CustomStoreConfiguration(AttributeSet attributes, AsyncStoreConfiguration async) {
-		super(attributes, async);
+		super(Element.STORE, attributes, async);
 	}
 
 }
