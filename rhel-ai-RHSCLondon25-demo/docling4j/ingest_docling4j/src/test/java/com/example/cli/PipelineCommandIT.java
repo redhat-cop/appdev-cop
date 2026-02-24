1b08cb079cf7ebdf6150fea128c16c9f474d0f47
pamenon <pamenon@redhat.com>
2026-02-24 23:04:28 +0000

    Added docling4j with quarkus cli example

---
+++ rhel-ai-RHSCLondon25-demo/docling4j/ingest_docling4j/src/test/java/com/example/cli/PipelineCommandIT.java
@@ -0,0 +1,8 @@
+package com.example.cli;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class PipelineCommandIT extends PipelineCommandTest {
+    // Execute the same tests but in packaged mode.
+}
