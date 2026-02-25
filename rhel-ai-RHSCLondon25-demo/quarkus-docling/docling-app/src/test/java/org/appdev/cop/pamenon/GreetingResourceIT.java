a628a1d3532ce485328de17dc916447ff4aa1264
pamenon <pamenon@redhat.com>
2026-02-25 12:19:23 +0000

    Inital quarkus docling-java project commit

---
+++ rhel-ai-RHSCLondon25-demo/quarkus-docling/docling-app/src/test/java/org/appdev/cop/pamenon/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.appdev.cop.pamenon;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
