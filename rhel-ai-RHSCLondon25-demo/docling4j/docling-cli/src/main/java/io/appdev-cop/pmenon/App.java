53664fe59cffeb815bc56fb78c7305c78ce577df
pamenon <pamenon@redhat.com>
2026-02-25 12:00:06 +0000

    Added plan java docling initial project

---
+++ rhel-ai-RHSCLondon25-demo/docling4j/docling-cli/src/main/java/io/appdev-cop/pmenon/App.java
@@ -0,0 +1,13 @@
+package io.appdev-cop.pmenon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
